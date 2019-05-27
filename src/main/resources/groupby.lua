684e28b635013928dba978be3f86827b38a05bd7
Alexander Radzin <alexander_radzin@yahoo.com>
2019-05-27 22:11:00 +0300

    groupby script is simplified: redundant group name is removed

--- src/main/resources/groupby.lua
+++ src/main/resources/groupby.lua
@@ -17,25 +17,25 @@ function groupby(stream, gname, aggr)
                 local ak = a[k]
                 local bk = b[k]
                 if func == 'count' and name == '*' then
-                    out[gname .. '.count(*)'] = ak[gname .. '.count(*)'] + bk[gname .. '.count(*)']
+                    out['count(*)'] = ak['count(*)'] + bk['count(*)']
                 end
                 if name ~= '*' then
                     if func == 'count' then
-                        out[gname .. '.count(' .. name .. ')'] = ak[gname .. '.count(' .. name .. ')'] + bk[gname .. '.count(' .. name .. ')']
+                        out['count(' .. name .. ')'] = ak['count(' .. name .. ')'] + bk['count(' .. name .. ')']
                     end
                     if func == 'sum' then
-                        out[gname .. '.sum(' .. name .. ')'] = ak[gname .. '.sum(' .. name .. ')'] + bk[gname .. '.sum(' .. name .. ')']
+                        out['sum(' .. name .. ')'] = ak['sum(' .. name .. ')'] + bk['sum(' .. name .. ')']
                     end
                     if func == 'min' then
-                        out[gname .. '.min(' .. name .. ')'] = (ak[gname .. '.min(' .. name .. ')'] > bk[gname .. '.min(' .. name .. ')'] and bk[gname .. '.min(' .. name .. ')']) or ak[gname .. '.min(' .. name .. ')']
+                        out['min(' .. name .. ')'] = (ak['min(' .. name .. ')'] > bk['min(' .. name .. ')'] and bk['min(' .. name .. ')']) or ak['min(' .. name .. ')']
                     end
                     if func == 'max' then
-                        out[gname .. '.max(' .. name .. ')'] = (ak[gname .. '.max(' .. name .. ')'] < bk[gname .. '.max(' .. name .. ')'] and bk[gname .. '.max(' .. name .. ')']) or ak[gname .. '.max(' .. name .. ')']
+                        out['max(' .. name .. ')'] = (ak['max(' .. name .. ')'] < bk['max(' .. name .. ')'] and bk['max(' .. name .. ')']) or ak['max(' .. name .. ')']
                     end
                     if func == 'avg' then
-                        local s = ak[gname .. '.sum(' .. name .. ')'] + bk[gname .. '.sum(' .. name .. ')']
-                        local c = ak[gname .. '.count(' .. name .. ')'] + bk[gname .. '.count(' .. name .. ')']
-                        out[gname .. '.avg(' .. name .. ')'] = s / c
+                        local s = ak['sum(' .. name .. ')'] + bk['sum(' .. name .. ')']
+                        local c = ak['count(' .. name .. ')'] + bk['count(' .. name .. ')']
+                        out['avg(' .. name .. ')'] = s / c
                     end
                 end
                 result[k] = out
@@ -49,7 +49,7 @@ function groupby(stream, gname, aggr)
                 result[k] = b[k]
                 local out = result[k]
                 if name ~= '*' and func == 'avg' then
-                    out[gname .. '.avg(' .. name .. ')'] = out[gname .. '.sum(' .. name .. ')'] / out[gname .. '.count(' .. name .. ')']
+                    out['avg(' .. name .. ')'] = out['sum(' .. name .. ')'] / out['count(' .. name .. ')']
                 end
             end
         end
@@ -65,7 +65,7 @@ function groupby(stream, gname, aggr)
         local stats = groups[grp] or map()
         if func == "count" and name == "*" then
             info("groupby count(*)", name)
-            stats[gname .. '.count(*)'] = (stats[gname .. 'count(*)'] or 0) + ((rec and 1 ) or 0)
+            stats['count(*)'] = (stats['count(*)'] or 0) + ((rec and 1 ) or 0)
         end
         local val = ((rec and rec[name]) or nil)
         if name ~= '*' then
@@ -73,19 +73,19 @@ function groupby(stream, gname, aggr)
             if val then
                 info("groupby into if 2: mapper(%s)", name)
                 if func == 'count' or func == 'avg' then
-                    stats[gname .. '.count(' .. name .. ')'] = (stats[gname .. '.count(' .. name .. ')'] or 0) + ((val and 1 ) or 0)
+                    stats['count(' .. name .. ')'] = (stats['count(' .. name .. ')'] or 0) + ((val and 1 ) or 0)
                 end
                 if func == 'sum' or func == 'avg' then
-                    stats[gname .. '.sum(' .. name .. ')'] = (stats[gname .. '.sum(' .. name .. ')'] or 0) + (val or 0)
+                    stats['sum(' .. name .. ')'] = (stats['sum(' .. name .. ')'] or 0) + (val or 0)
                 end
                 if func == 'sumsqs' then
-                    stats[gname .. '.sumsqs(' .. name .. ')'] = (stats[gname .. '.sumsqs(' .. name .. ')'] or 0) + (val ^ 2)
+                    stats['sumsqs(' .. name .. ')'] = (stats['sumsqs(' .. name .. ')'] or 0) + (val ^ 2)
                 end
                 if func == 'min' then
-                    stats[gname .. '.min(' .. name .. ')'] = (not stats[gname .. '.min(' .. name .. ')'] and val) or (stats[gname .. '.min(' .. name .. ')'] and val < stats[gname .. '.min(' .. name .. ')'] and val) or stats['min(' .. name .. ')']
+                    stats['min(' .. name .. ')'] = (not stats['min(' .. name .. ')'] and val) or (stats['min(' .. name .. ')'] and val < stats['min(' .. name .. ')'] and val) or stats['min(' .. name .. ')']
                 end
                 if func == 'max' then
-                    stats[gname .. '.max(' .. name .. ')'] = (not stats[gname .. '.max(' .. name .. ')'] and val) or (stats[gname .. '.max(' .. name .. ')'] and val > stats[gname .. '.max(' .. name .. ')'] and val) or stats['max(' .. name .. ')']
+                    stats['max(' .. name .. ')'] = (not stats['max(' .. name .. ')'] and val) or (stats['max(' .. name .. ')'] and val > stats['max(' .. name .. ')'] and val) or stats['max(' .. name .. ')']
                 end
             end
         end
