30523138f3dd1c75fa27b04153c8e154d51060c3
Alexander Radzin <alexander_radzin@yahoo.com>
2019-10-27 01:25:25 +0300

    more mocked tests of AerospikeSqlClient

--- src/test/java/com/nosqldriver/aerospike/sql/AerospikeSqlClientTest.java
+++ src/test/java/com/nosqldriver/aerospike/sql/AerospikeSqlClientTest.java
@@ -2,6 +2,7 @@ package com.nosqldriver.aerospike.sql;
 
 import com.aerospike.client.AerospikeClient;
 import com.aerospike.client.AerospikeException;
+import com.aerospike.client.Bin;
 import com.aerospike.client.IAerospikeClient;
 import com.aerospike.client.Key;
 import com.aerospike.client.Language;
@@ -35,10 +36,10 @@ import org.mockito.ArgumentMatchers;
 
 import java.sql.SQLException;
 import java.util.Calendar;
-import java.util.Collections;
 
 import static com.nosqldriver.aerospike.sql.TestDataUtils.NAMESPACE;
 import static com.nosqldriver.aerospike.sql.TestDataUtils.PEOPLE;
+import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -47,6 +48,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 class AerospikeSqlClientTest {
     @Test
@@ -100,6 +103,188 @@ class AerospikeSqlClientTest {
         assertCallOnClosedClient(wrapperClient, SQLException.class);
     }
 
+    @Test
+    void delegation() {
+        IAerospikeClient mock = mock(IAerospikeClient.class);
+        IAerospikeClient wrapperClient = new AerospikeSqlClient(() -> mock);
+
+        Key key = new Key(NAMESPACE, PEOPLE, "KEY");
+        WritePolicy wp = new WritePolicy();
+        InfoPolicy ip = new InfoPolicy();
+        BatchPolicy bp = new BatchPolicy();
+        Policy p = new Policy();
+        ScanPolicy sp = new ScanPolicy();
+        QueryPolicy qp = new QueryPolicy();
+        AdminPolicy ap = new AdminPolicy();
+        EventLoop eventLoop = mock(EventLoop.class);
+        WriteListener writeListener = mock(WriteListener.class);
+        DeleteListener deleteListener = mock(DeleteListener.class);
+        ExistsListener existsListener = mock(ExistsListener.class);
+        ExistsSequenceListener existsSequenceListener = mock(ExistsSequenceListener.class);
+        ExistsArrayListener existsArrayListener = mock(ExistsArrayListener.class);
+        RecordListener recordListener = mock(RecordListener.class);
+        BatchSequenceListener batchSequenceListener = mock(BatchSequenceListener.class);
+        RecordArrayListener recordArrayListener = mock(RecordArrayListener.class);
+        RecordSequenceListener recordSequenceListener = mock(RecordSequenceListener.class);
+        ExecuteListener executeListener = mock(ExecuteListener.class);
+        BatchListListener batchListListener = mock(BatchListListener.class);
+
+        wrapperClient.put(wp, key);
+        verify(mock, times(1)).put(wp, key);
+
+        wrapperClient.put(eventLoop, writeListener, wp, key);
+        verify(mock, times(1)).put(eventLoop, writeListener, wp, key);
+
+        wrapperClient.append(wp, key);
+        verify(mock, times(1)).append(wp, key);
+
+        wrapperClient.append(eventLoop, writeListener, wp, key);
+        verify(mock, times(1)).append(eventLoop, writeListener, wp, key);
+
+        wrapperClient.prepend(wp, key);
+        verify(mock, times(1)).prepend(wp, key);
+
+        wrapperClient.prepend(eventLoop, writeListener, wp, key);
+        verify(mock, times(1)).prepend(eventLoop, writeListener, wp, key);
+
+        wrapperClient.add(wp, key);
+        verify(mock, times(1)).add(wp, key);
+
+        wrapperClient.add(eventLoop, writeListener, wp, key);
+        verify(mock, times(1)).add(eventLoop, writeListener, wp, key);
+
+        wrapperClient.delete(wp, key);
+        verify(mock, times(1)).delete(wp, key);
+
+        wrapperClient.delete(eventLoop, deleteListener, wp, key);
+        verify(mock, times(1)).delete(eventLoop, deleteListener, wp, key);
+
+        Calendar calendar = Calendar.getInstance();
+        wrapperClient.truncate(ip, "ns", "set", calendar);
+        verify(mock, times(1)).truncate(ip, "ns", "set", calendar);
+
+
+        wrapperClient.touch(wp, key);
+        verify(mock, times(1)).touch(wp, key);
+
+        wrapperClient.touch(eventLoop, writeListener, wp, key);
+        verify(mock, times(1)).touch(eventLoop, writeListener, wp, key);
+
+        wrapperClient.exists(wp, key);
+        verify(mock, times(1)).exists(wp, key);
+
+        wrapperClient.exists(eventLoop, existsListener, wp, key);
+        verify(mock, times(1)).exists(eventLoop, existsListener, wp, key);
+
+        Key[] keys = new Key[] {key};
+        wrapperClient.exists(bp, keys);
+        verify(mock, times(1)).exists(bp, keys);
+
+        wrapperClient.exists(eventLoop, existsSequenceListener, bp, keys);
+        verify(mock, times(1)).exists(eventLoop, existsSequenceListener, bp, keys);
+
+        wrapperClient.exists(eventLoop, existsArrayListener, bp, keys);
+        verify(mock, times(1)).exists(eventLoop, existsArrayListener, bp, keys);
+
+
+        wrapperClient.get(eventLoop, recordListener, p, key);
+        verify(mock, times(1)).get(eventLoop, recordListener, p, key);
+
+        String[] bins = {"bin1", "bin2"};
+        wrapperClient.get(eventLoop, recordListener, p, key, bins);
+        verify(mock, times(1)).get(eventLoop, recordListener, p, key, bins);
+
+
+        wrapperClient.get(wp, key);
+        verify(mock, times(1)).get(wp, key);
+
+        wrapperClient.get(eventLoop, batchSequenceListener, bp, emptyList());
+        verify(mock, times(1)).get(eventLoop, batchSequenceListener, bp, emptyList());
+
+        wrapperClient.get(eventLoop, recordArrayListener, bp, keys);
+        verify(mock, times(1)).get(eventLoop, recordArrayListener, bp, keys);
+
+        wrapperClient.get(eventLoop, recordArrayListener, bp, keys, bins);
+        verify(mock, times(1)).get(eventLoop, recordArrayListener, bp, keys, bins);
+
+
+        wrapperClient.getHeader(eventLoop, recordArrayListener, bp, keys);
+        verify(mock, times(1)).getHeader(eventLoop, recordArrayListener, bp, keys);
+
+        wrapperClient.getHeader(eventLoop, recordSequenceListener, bp, keys);
+        verify(mock, times(1)).getHeader(eventLoop, recordSequenceListener, bp, keys);
+
+        wrapperClient.getHeader(eventLoop, recordListener, p, key);
+        verify(mock, times(1)).getHeader(eventLoop, recordListener, p, key);
+
+        wrapperClient.operate(eventLoop, recordListener, wp, key);
+        verify(mock, times(1)).operate(eventLoop, recordListener, wp, key);
+
+        ScanCallback scanCallback = mock(ScanCallback.class);
+        wrapperClient.scanAll(sp, "namespace", "set", scanCallback);
+        verify(mock, times(1)).scanAll(sp, "namespace", "set", scanCallback);
+
+        wrapperClient.scanAll(eventLoop, recordSequenceListener, sp, "namespace", "set");
+        verify(mock, times(1)).scanAll(eventLoop, recordSequenceListener, sp, "namespace", "set");
+
+        wrapperClient.scanNode(sp, "node", "namespace", "set", scanCallback);
+        verify(mock, times(1)).scanNode(sp, "node", "namespace", "set", scanCallback);
+
+        Node node = mock(Node.class);
+        wrapperClient.scanNode(sp, node, "namespace", "set", scanCallback);
+        verify(mock, times(1)).scanNode(sp, node, "namespace", "set", scanCallback);
+
+
+        wrapperClient.removeUdf(ip, "path");
+        verify(mock, times(1)).removeUdf(ip, "path");
+
+        wrapperClient.execute(eventLoop, executeListener, wp, key, "package", "function");
+        verify(mock, times(1)).execute(eventLoop, executeListener, wp, key, "package", "function");
+
+        Statement statement = new Statement();
+        wrapperClient.query(eventLoop, recordSequenceListener, qp, statement);
+        verify(mock, times(1)).query(eventLoop, recordSequenceListener, qp, statement);
+
+
+        wrapperClient.createUser(ap, "user", "password", emptyList());
+        verify(mock, times(1)).createUser(ap, "user", "password", emptyList());
+
+        wrapperClient.dropUser(ap, "user");
+        verify(mock, times(1)).dropUser(ap, "user");
+
+        wrapperClient.changePassword(ap, "user", "password");
+        verify(mock, times(1)).changePassword(ap, "user", "password");
+
+        wrapperClient.grantRoles(ap, "user", emptyList());
+        verify(mock, times(1)).grantRoles(ap, "user", emptyList());
+
+        wrapperClient.revokeRoles(ap, "user", emptyList());
+        verify(mock, times(1)).revokeRoles(ap, "user", emptyList());
+
+        wrapperClient.createRole(ap, "role", emptyList());
+        verify(mock, times(1)).createRole(ap, "role", emptyList());
+
+        wrapperClient.dropRole(ap, "role");
+        verify(mock, times(1)).dropRole(ap, "role");
+
+        wrapperClient.grantPrivileges(ap, "role", emptyList());
+        verify(mock, times(1)).grantPrivileges(ap, "role", emptyList());
+
+        wrapperClient.revokePrivileges(ap, "role", emptyList());
+        verify(mock, times(1)).revokePrivileges(ap, "role", emptyList());
+
+        wrapperClient.get(bp, emptyList());
+        verify(mock, times(1)).get(bp, emptyList());
+
+        wrapperClient.get(eventLoop, batchListListener, bp, emptyList());
+        verify(mock, times(1)).get(bp, emptyList());
+
+        wrapperClient.get(eventLoop, recordSequenceListener, bp, keys);
+        verify(mock, times(1)).get(eventLoop, recordSequenceListener, bp, keys);
+
+        wrapperClient.get(eventLoop, recordSequenceListener, bp, keys, bins);
+        verify(mock, times(1)).get(eventLoop, recordSequenceListener, bp, keys, bins);
+    }
 
     @Test
     void exceptions() {
@@ -112,26 +297,47 @@ class AerospikeSqlClientTest {
         doThrow(ase).when(mock).put(any(WritePolicy.class), any(Key.class));
         assertThrows(SQLException.class, () -> wrapperClient.put(new WritePolicy(), key));
 
+        doThrow(ase).when(mock).put(any(WritePolicy.class), any(Key.class), any());
+        assertThrows(SQLException.class, () -> wrapperClient.put(new WritePolicy(), key, new Bin("name", "value")));
+
         doThrow(ase).when(mock).put(any(EventLoop.class), any(WriteListener.class), any(WritePolicy.class), any(Key.class));
         assertThrows(SQLException.class, () -> wrapperClient.put(mock(EventLoop.class), mock(WriteListener.class), new WritePolicy(), key));
 
         doThrow(ase).when(mock).append(any(WritePolicy.class), any(Key.class));
         assertThrows(SQLException.class, () -> wrapperClient.append(new WritePolicy(), key));
 
+        doThrow(ase).when(mock).put(any(WritePolicy.class), any(Key.class), any());
+        assertThrows(SQLException.class, () -> wrapperClient.put(new WritePolicy(), key, new Bin("name", "value")));
+
         doThrow(ase).when(mock).append(any(EventLoop.class), any(WriteListener.class), any(WritePolicy.class), any(Key.class));
         assertThrows(SQLException.class, () -> wrapperClient.append(mock(EventLoop.class), mock(WriteListener.class), new WritePolicy(), key));
+
+        doThrow(ase).when(mock).append(any(EventLoop.class), any(WriteListener.class), any(WritePolicy.class), any(Key.class), any());
+        assertThrows(SQLException.class, () -> wrapperClient.append(mock(EventLoop.class), mock(WriteListener.class), new WritePolicy(), key, new Bin("name", "value")));
 
         doThrow(ase).when(mock).prepend(any(WritePolicy.class), any(Key.class));
         assertThrows(SQLException.class, () -> wrapperClient.prepend(new WritePolicy(), key));
 
+        doThrow(ase).when(mock).prepend(any(WritePolicy.class), any(Key.class), any());
+        assertThrows(SQLException.class, () -> wrapperClient.prepend(new WritePolicy(), key, new Bin("name", "value")));
+
         doThrow(ase).when(mock).prepend(any(EventLoop.class), any(WriteListener.class), any(WritePolicy.class), any(Key.class));
         assertThrows(SQLException.class, () -> wrapperClient.prepend(mock(EventLoop.class), mock(WriteListener.class), new WritePolicy(), key));
+
+        doThrow(ase).when(mock).prepend(any(EventLoop.class), any(WriteListener.class), any(WritePolicy.class), any(Key.class), any());
+        assertThrows(SQLException.class, () -> wrapperClient.prepend(mock(EventLoop.class), mock(WriteListener.class), new WritePolicy(), key, new Bin("name", "value")));
 
         doThrow(ase).when(mock).add(any(WritePolicy.class), any(Key.class));
         assertThrows(SQLException.class, () -> wrapperClient.add(new WritePolicy(), key));
 
+        doThrow(ase).when(mock).add(any(WritePolicy.class), any(Key.class), any());
+        assertThrows(SQLException.class, () -> wrapperClient.add(new WritePolicy(), key, new Bin("name", "value")));
+
         doThrow(ase).when(mock).add(any(EventLoop.class), any(WriteListener.class), any(WritePolicy.class), any(Key.class));
         assertThrows(SQLException.class, () -> wrapperClient.add(mock(EventLoop.class), mock(WriteListener.class), new WritePolicy(), key));
+
+        doThrow(ase).when(mock).add(any(EventLoop.class), any(WriteListener.class), any(WritePolicy.class), any(Key.class), any());
+        assertThrows(SQLException.class, () -> wrapperClient.add(mock(EventLoop.class), mock(WriteListener.class), new WritePolicy(), key, new Bin("name", "value")));
 
         doThrow(ase).when(mock).delete(any(WritePolicy.class), any(Key.class));
         assertThrows(SQLException.class, () -> wrapperClient.delete(new WritePolicy(), key));
@@ -185,13 +391,13 @@ class AerospikeSqlClientTest {
         assertThrows(SQLException.class, () -> wrapperClient.getHeader(mock(EventLoop.class), mock(RecordListener.class), new Policy(), key));
 
         doThrow(ase).when(mock).get(any(BatchPolicy.class), ArgumentMatchers.anyList());
-        assertThrows(SQLException.class, () -> wrapperClient.get(new BatchPolicy(), Collections.emptyList()));
+        assertThrows(SQLException.class, () -> wrapperClient.get(new BatchPolicy(), emptyList()));
 
         doThrow(ase).when(mock).get(any(EventLoop.class), any(BatchListListener.class), any(BatchPolicy.class), ArgumentMatchers.anyList());
-        assertThrows(SQLException.class, () -> wrapperClient.get(mock(EventLoop.class), mock(BatchListListener.class), new BatchPolicy(), Collections.emptyList()));
+        assertThrows(SQLException.class, () -> wrapperClient.get(mock(EventLoop.class), mock(BatchListListener.class), new BatchPolicy(), emptyList()));
 
         doThrow(ase).when(mock).get(any(EventLoop.class), any(BatchSequenceListener.class), any(BatchPolicy.class), ArgumentMatchers.anyList());
-        assertThrows(SQLException.class, () -> wrapperClient.get(mock(EventLoop.class), mock(BatchSequenceListener.class), new BatchPolicy(), Collections.emptyList()));
+        assertThrows(SQLException.class, () -> wrapperClient.get(mock(EventLoop.class), mock(BatchSequenceListener.class), new BatchPolicy(), emptyList()));
 
         doThrow(ase).when(mock).get(any(BatchPolicy.class), any(Key[].class));
         assertThrows(SQLException.class, () -> wrapperClient.get(new BatchPolicy(), new Key[] {key}));
@@ -307,7 +513,7 @@ class AerospikeSqlClientTest {
 
 
         doThrow(ase).when(mock).createUser(any(AdminPolicy.class), eq("user"), eq("password1"), ArgumentMatchers.anyList());
-        assertThrows(SQLException.class, () -> wrapperClient.createUser(new AdminPolicy(), "user", "password1", Collections.emptyList()));
+        assertThrows(SQLException.class, () -> wrapperClient.createUser(new AdminPolicy(), "user", "password1", emptyList()));
 
         doThrow(ase).when(mock).dropUser(any(AdminPolicy.class), eq("user"));
         assertThrows(SQLException.class, () -> wrapperClient.dropUser(new AdminPolicy(), "user"));
@@ -317,23 +523,23 @@ class AerospikeSqlClientTest {
 
 
         doThrow(ase).when(mock).grantRoles(any(AdminPolicy.class), eq("user"), ArgumentMatchers.anyList());
-        assertThrows(SQLException.class, () -> wrapperClient.grantRoles(new AdminPolicy(), "user", Collections.emptyList()));
+        assertThrows(SQLException.class, () -> wrapperClient.grantRoles(new AdminPolicy(), "user", emptyList()));
 
         doThrow(ase).when(mock).revokeRoles(any(AdminPolicy.class), eq("user"), ArgumentMatchers.anyList());
-        assertThrows(SQLException.class, () -> wrapperClient.revokeRoles(new AdminPolicy(), "user", Collections.emptyList()));
+        assertThrows(SQLException.class, () -> wrapperClient.revokeRoles(new AdminPolicy(), "user", emptyList()));
 
         doThrow(ase).when(mock).createRole(any(AdminPolicy.class), eq("role1"), ArgumentMatchers.anyList());
-        assertThrows(SQLException.class, () -> wrapperClient.createRole(new AdminPolicy(), "role1", Collections.emptyList()));
+        assertThrows(SQLException.class, () -> wrapperClient.createRole(new AdminPolicy(), "role1", emptyList()));
 
 
         doThrow(ase).when(mock).dropRole(any(AdminPolicy.class), eq("role1"));
         assertThrows(SQLException.class, () -> wrapperClient.dropRole(new AdminPolicy(), "role1"));
 
         doThrow(ase).when(mock).grantPrivileges(any(AdminPolicy.class), eq("role1"), ArgumentMatchers.anyList());
-        assertThrows(SQLException.class, () -> wrapperClient.grantPrivileges(new AdminPolicy(), "role1", Collections.emptyList()));
+        assertThrows(SQLException.class, () -> wrapperClient.grantPrivileges(new AdminPolicy(), "role1", emptyList()));
 
         doThrow(ase).when(mock).revokePrivileges(any(AdminPolicy.class), eq("role1"), ArgumentMatchers.anyList());
-        assertThrows(SQLException.class, () -> wrapperClient.revokePrivileges(new AdminPolicy(), "role1", Collections.emptyList()));
+        assertThrows(SQLException.class, () -> wrapperClient.revokePrivileges(new AdminPolicy(), "role1", emptyList()));
 
         doThrow(ase).when(mock).queryUser(any(AdminPolicy.class), eq("user1"));
         assertThrows(SQLException.class, () -> wrapperClient.queryUser(new AdminPolicy(), "user1"));
