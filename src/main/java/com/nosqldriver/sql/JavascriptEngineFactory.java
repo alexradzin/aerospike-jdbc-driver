249bc1ae5d2ea05939fc51b142a07bb539cab54d
Alexander Radzin <alexander_radzin@yahoo.com>
2019-12-29 12:49:11 +0200

    Nahshort scripting engine is now cached in ThreadLocal

--- src/main/java/com/nosqldriver/sql/JavascriptEngineFactory.java
+++ src/main/java/com/nosqldriver/sql/JavascriptEngineFactory.java
@@ -1,5 +1,6 @@
 package com.nosqldriver.sql;
 
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -14,11 +15,21 @@ import static java.lang.String.format;
 
 public class JavascriptEngineFactory {
     private static final Pattern FUNCTION_HEADER = Pattern.compile("function\\s+(\\w+)\\s*\\(");
+    private static final ThreadLocal<ScriptEngine> threadEngine = new ThreadLocal<>();
     private final ScriptEngine engine;
 
     public JavascriptEngineFactory() {
         try {
-            engine = new ScriptEngineManager().getEngineByName("JavaScript");
+            synchronized (threadEngine) {
+                ScriptEngine tmp = threadEngine.get();
+                if (tmp == null) {
+                    engine = new ScriptEngineManager().getEngineByName("JavaScript");
+                    threadEngine.set(engine);
+                } else {
+                    engine = tmp;
+                    engine.getBindings(ScriptContext.ENGINE_SCOPE).clear();
+                }
+            }
             Reader functions = new InputStreamReader(getClass().getResourceAsStream("/functions.js"));
             String allFunctionsSrc = new BufferedReader(functions).lines().collect(Collectors.joining("\n"));
             Matcher matcher = FUNCTION_HEADER.matcher(allFunctionsSrc);
