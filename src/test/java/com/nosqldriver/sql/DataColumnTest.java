0bb118e3606cc862a0d06bafec3e18a1b692db68
Alexander Radzin <alexander_radzin@yahoo.com>
2020-01-18 21:11:28 +0200

    improved test of DataColumn

--- src/test/java/com/nosqldriver/sql/DataColumnTest.java
+++ src/test/java/com/nosqldriver/sql/DataColumnTest.java
@@ -2,7 +2,10 @@ package com.nosqldriver.sql;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Types;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class DataColumnTest {
     @Test
@@ -30,6 +33,12 @@ class DataColumnTest {
         assertNotEquals(DataColumn.DataColumnRole.DATA.create("cat1", "tab", "col", "lab"), DataColumn.DataColumnRole.DATA.create("cat2", "tab", "col", "lab"));
         assertNotEquals(DataColumn.DataColumnRole.DATA.create("cat", "tab1", "col", "lab"), DataColumn.DataColumnRole.DATA.create("cat", "tab2", "col", "lab"));
         assertNotEquals(DataColumn.DataColumnRole.DATA.create("cat", "tab", "col1", "lab"), DataColumn.DataColumnRole.DATA.create("cat", "tab", "col2", "lab"));
-        assertNotEquals(DataColumn.DataColumnRole.DATA.create("cat", "tab", "col1", "lab1"), DataColumn.DataColumnRole.DATA.create("cat", "tab", "col2", "lab2"));
+        assertNotEquals(DataColumn.DataColumnRole.DATA.create("cat", "tab", "col", "lab1"), DataColumn.DataColumnRole.DATA.create("cat", "tab", "col", "lab2"));
+        assertNotEquals(DataColumn.DataColumnRole.EXPRESSION.create("cat", "tab", "1+1", "lab"), DataColumn.DataColumnRole.DATA.create("cat", "tab", "2*2", "lab"));
+
+        assertNotEquals(DataColumn.DataColumnRole.DATA.create("cat", "tab", "col", "lab"), null);
+        assertNotEquals(DataColumn.DataColumnRole.DATA.create("cat", "tab", "col", "lab"), "");
+        assertNotEquals(DataColumn.DataColumnRole.DATA.create("cat", "tab", "col1", "lab1").withType(Types.INTEGER), DataColumn.DataColumnRole.DATA.create("cat", "tab", "col1", "lab1").withType(Types.BIGINT));
+
     }
 }
