b069f8235e97d3ed036077034e751ea5c096e5cb
Alexander Radzin <alexander_radzin@yahoo.com>
2020-03-12 07:51:21 +0200

    fixed typo

--- src/test/java/com/nosqldriver/aerospike/sql/package-info.java
+++ src/test/java/com/nosqldriver/aerospike/sql/package-info.java
@@ -1,6 +1,6 @@
 /**
  * This package contains tests that can be qualified as integration rather than unit tests because they peform real
- * SQL requests agains real Aerospike DB running locally using the JDBC driver developled here.
+ * SQL requests against real Aerospike DB running locally using the JDBC driver developled here.
  *
  * The test are grouped in test cases by the main SQL statement (SELECT, UPDATE, DELETE, INSERT etc.) as well as
  * by the data required for the test. This is done for performance reasons. There are a lot of read-only tests
